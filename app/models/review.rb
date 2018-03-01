ef5eefb2bdb7c6ae757af0149403b9b928bf1dd3
Raghu Betina <raghu@firstdraft.com>
2018-03-01 20:45:58 +0000

    Validate presence of reviews.rating

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,6 +5,8 @@ class Review < ApplicationRecord
 
   # Validations
 
+  validates :rating, :presence => true
+
   validates :rating, :numericality => { :less_than_or_equal_to => 5, :greater_than_or_equal_to => 1 }
 
 end
