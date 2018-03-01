d30e6bf74be07b991de9b69408f4aac5c0106206
Raghu Betina <raghu@firstdraft.com>
2018-03-01 20:46:08 +0000

    Indirect association between users and restaurants

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -12,6 +12,10 @@ class Restaurant < ApplicationRecord
 
   # Indirect associations
 
+  has_many   :users,
+             :through => :reviews,
+             :source => :user
+
   # Validations
 
 end
