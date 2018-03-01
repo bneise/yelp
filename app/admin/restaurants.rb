79a264238b46d2ca4521d1eee62417d5c576c1d3
Raghu Betina <raghu@firstdraft.com>
2018-03-01 20:46:26 +0000

    Whitelist columns in Restaurant for ActiveAdmin

--- app/admin/restaurants.rb
+++ app/admin/restaurants.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Restaurant do
+
+ permit_params :name, :address, :city, :neighborhood, :short_description, :hours, :website, :total_ratings, :num_reviews
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
